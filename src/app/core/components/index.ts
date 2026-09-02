97072170e08a29e5447fce899ce26f60962cf62f
Stefan Tataran <fane_ecl@yahoo.com>
2026-09-02 21:18:54 +0300

    Initial barrels

---
+++ src/app/core/components/index.ts
@@ -0,0 +1,7 @@
+// Confirmation Dialog
+export { ConfirmationDialog } from './confirmation-dialog/confirmation-dialog';
+export type { ConfirmationDialogData } from './confirmation-dialog/confirmation-dialog';
+export { ConfirmationDialogService } from './confirmation-dialog/confirmation-dialog.service'
+
+// Enclave Avatar
+export { EnclaveAvatar } from './enclave-avatar/enclave-avatar'
---
+++ src/app/core/index.ts
@@ -0,0 +1,8 @@
+// Theme
+export { ThemeService } from './theme/theme.service';
+export type { Theme } from './theme/theme.service';
+
+// Routing
+export { EnclaveTitleStrategy } from './routing/enclave-title-strategy';
+
+// 
