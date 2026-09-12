4cc8f4655fa83de2f9aba8d25350a6871b356a8a
Stefan Tataran <fane_ecl@yahoo.com>
2026-09-12 10:15:39 +0300

    [Feat] Implement org details card (#27)

--- src/app/features/admin/organizations/organization-details/organization-details.ts
+++ src/app/features/admin/organizations/organization-details/organization-details.ts
@@ -1,11 +1,28 @@
 import { ChangeDetectionStrategy, Component, computed, inject, input } from '@angular/core';
+import { MatTabGroup, MatTab } from '@angular/material/tabs';
 
-import { EnclaveDetailsHeader } from '@enclave/core/components';
+import {
+  EnclaveDetailsHeader,
+  EnclaveDetailCard,
+  EnclaveDetailList,
+  EnclaveDetailRow,
+  EnclaveStatus,
+} from '@enclave/core/components';
+import { EnclavePersistentTab } from '@enclave/core/directives';
 import { OrganizationService, UserService } from '@enclave/domain/services';
 
 @Component({
   selector: 'enclave-organization-details',
-  imports: [EnclaveDetailsHeader],
+  imports: [
+    EnclaveDetailCard,
+    EnclaveDetailList,
+    EnclaveDetailRow,
+    EnclaveDetailsHeader,
+    EnclavePersistentTab,
+    EnclaveStatus,
+    MatTab,
+    MatTabGroup,
+  ],
   templateUrl: './organization-details.html',
   styleUrl: './organization-details.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
